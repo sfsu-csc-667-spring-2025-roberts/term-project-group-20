663d8207ad028080fc25d9c17588c4572d59402f
haodeng08 <haodeng08@gmail.com>
2025-05-21 17:20:46 -0700

    define cards in game

---
+++ src/client/games/card.ts
@@ -0,0 +1,30 @@
+export enum CardColor {
+    RED = 'red',
+    BLUE = 'blue',
+    GREEN = 'green',
+    YELLOW = 'yellow',
+    WILD = 'wild'
+  }
+  
+  export enum CardType {
+    // Number cards
+    ZERO = '0',
+    ONE = '1',
+    TWO = '2',
+    THREE = '3',
+    FOUR = '4',
+    FIVE = '5',
+    SIX = '6',
+    SEVEN = '7',
+    EIGHT = '8',
+    NINE = '9',
+    
+    // Action cards
+    SKIP = 'skip',
+    REVERSE = 'reverse',
+    DRAW_TWO = 'draw_two',
+    
+    // Wild cards
+    WILD = 'wild',
+    WILD_DRAW_FOUR = 'wild_draw_four'
+  }
